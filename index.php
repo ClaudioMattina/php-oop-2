88cc285c5b634f4e22a102e8990075f0b847167a
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-05-14 18:23:32 +0200

    inserimento di costumer

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once __DIR__ .  '/Costumer.php';
 
 $ciccia = new Food("carne", "12.30", "carne per cani molto buona", "pollo");
 $myCard = new CreditCard(123544657687967564, 222, 2029);
+$kukku = new Costumer("kukku", "kukkunino", 26, $myCard,false,"no");
 
 echo "<pre>";
 
@@ -15,6 +16,12 @@ echo "</pre>";
 echo "<pre>";
 
     var_dump($myCard);
+
+echo "</pre>";
+
+echo "<pre>";
+
+    var_dump($kukku);
 
 echo "</pre>";
 
