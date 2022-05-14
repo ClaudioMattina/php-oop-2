3766ad541df69cf342e915966ed7df79a4741162
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-05-14 18:08:55 +0200

    test con ciccia

---
+++ costumer.php
@@ -0,0 +1,54 @@
+<?php
+require_once __DIR__ . 'CreditCard.php';
+
+class Costumer{
+    public $name;
+    public $last_name;
+    public $age;
+    public $credit_card;
+    public $is_register;
+    public $discount;
+
+    public function __construct($name,$last_name,$age,$credit_card,$is_register,$discount)
+    {
+        $this->name=$name;
+        $this->last_name=$last_name;
+        $this->age=$age;
+        $this->credit_card=$credit_card;
+        $this->is_register=$is_register;
+        $this->discount=$discount;
+    }
+
+    public function getDiscount(){
+        return "Hai diritto al " . $this->discount . "% di sconto.";
+    }
+
+    public function setDiscount(){
+       return  $this-> is_register ? $this-> discount = 20 : $this->discount = 0;
+    }
+
+    public function setCreditCard($credit_card){
+
+        /* da ripassare */
+        if(!$credit_card instanceof CreditCard) return false;
+        $this->credit_card = $credit_card;
+    }
+
+    public function getName(){
+        return $this-> name;
+    }
+
+    public function getLastName(){
+        return $this-> last_name;
+    }
+
+    public function getAge(){
+        return $this-> age;
+    }
+
+    public function getIsRegister(){
+        return $this-> is_register;
+    }
+
+}
+?>
--- index.php
+++ index.php
@@ -2,4 +2,9 @@
 require_once __DIR__ .  'Food.php';
 require_once __DIR__ .  'Costumer.php';
 
+
+$ciccia = new Food('carne', 12.30, 'carne per cani molto buona', 'pollo');
+
+echo $ciccia
 ?>
+
